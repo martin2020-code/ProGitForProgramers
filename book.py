b1fb5e595a0567dbf2edf0231e7bbcc005624e1c
martin2020-code <82909403+martin2020-code@users.noreply.github.com>
2022-09-06 16:27:49 -0500

    Add book class

---
+++ book.py
@@ -0,0 +1,6 @@
+class Book:
+    
+    def __init__(self, title, actors, publication_date) -> None:
+        self.title = title
+        self.actors = actors
+        self.publication_date = publication_date 
